1fadb77f4c31c7c4fa7ef9b1a4d5b749983d74f9
Katie Gaines <klgaines18@gmail.com>
2023-04-15 07:38:56 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 //Routes
