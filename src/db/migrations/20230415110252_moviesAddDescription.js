7c3c3e352f12e420772a9d31cf65367f02a1d662
Katie Gaines <klgaines18@gmail.com>
2023-04-15 07:05:23 -0400

    add description column to movies table

---
+++ src/db/migrations/20230415110252_moviesAddDescription.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table("movies", (table) => {
+    table.text("description");
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table("movies", (table) => {
+    table.dropColumn("description");
+  })
+};
