9093367d501d24873387021a976cd068f6b47bf9
Katie Gaines <klgaines18@gmail.com>
2023-04-15 07:58:50 -0400

    add movies route to app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
-const app = express();
+const moviesRouter = require("./movies/movies.router");
 
 app.use(cors());
 app.use(express.json());
 
 //Routes
+app.use("/movies", moviesRouter)
 
 // Error handlers
 app.use(notFound);
