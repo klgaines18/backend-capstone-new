b5eaa378f6383a4d894f49f2d5ea06cef94d4921
Katie Gaines <klgaines18@gmail.com>
2023-04-17 10:28:15 -0400

    updated formatting with prettier

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -5,18 +5,12 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 const reviewsRouter = require("../reviews/reviews.router");
 const theatersRouter = require("../theaters/theaters.router");
 
-// need to add validation
+
 router.use("/:movieId/reviews", controller.movieExists, reviewsRouter);
 router.use("/:movieId/theaters", controller.movieExists, theatersRouter);
 
-router
-  .route("/:movieId")
-  .get(controller.read)
-  .all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 
-router
-  .route("/")
-  .get(controller.list)
-  .all(methodNotAllowed);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
